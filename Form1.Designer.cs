92418cf670414eda2dd08a8d4223979b3bb85f63
taerd <ffifalafiesta@gmail.com>
2020-02-24 21:32:00 +0300

    Добавил функционал

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,20 +31,30 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panelMain = new System.Windows.Forms.Panel();
             this.groupBoxTools = new System.Windows.Forms.GroupBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxBrush = new System.Windows.Forms.PictureBox();
             this.pictureEraser = new System.Windows.Forms.PictureBox();
             this.picturePen = new System.Windows.Forms.PictureBox();
             this.groupBoxColor = new System.Windows.Forms.GroupBox();
+            this.panelPurple = new System.Windows.Forms.Panel();
+            this.panelYellow = new System.Windows.Forms.Panel();
+            this.panelGreen = new System.Windows.Forms.Panel();
             this.panelBlue = new System.Windows.Forms.Panel();
             this.panelRed = new System.Windows.Forms.Panel();
             this.panelBlack = new System.Windows.Forms.Panel();
             this.comboBoxPtt = new System.Windows.Forms.ComboBox();
             this.buttonClear = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBoxLine = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRectangle = new System.Windows.Forms.PictureBox();
+            this.buttonSave = new System.Windows.Forms.Button();
             this.groupBoxTools.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBrush)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureEraser)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picturePen)).BeginInit();
             this.groupBoxColor.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLine)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRectangle)).BeginInit();
             this.SuspendLayout();
             // 
             // panelMain
@@ -55,15 +65,16 @@
             this.panelMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panelMain.Location = new System.Drawing.Point(13, 97);
             this.panelMain.Name = "panelMain";
-            this.panelMain.Size = new System.Drawing.Size(707, 343);
+            this.panelMain.Size = new System.Drawing.Size(1507, 598);
             this.panelMain.TabIndex = 0;
+            this.panelMain.Paint += new System.Windows.Forms.PaintEventHandler(this.panelMain_Paint);
             this.panelMain.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelMain_MouseDown);
             this.panelMain.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelMain_MouseMove);
             this.panelMain.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelMain_MouseUp);
             // 
             // groupBoxTools
             // 
-            this.groupBoxTools.Controls.Add(this.pictureBox3);
+            this.groupBoxTools.Controls.Add(this.pictureBoxBrush);
             this.groupBoxTools.Controls.Add(this.pictureEraser);
             this.groupBoxTools.Controls.Add(this.picturePen);
             this.groupBoxTools.Location = new System.Drawing.Point(13, 13);
@@ -73,15 +84,16 @@
             this.groupBoxTools.TabStop = false;
             this.groupBoxTools.Text = "Инструменты";
             // 
-            // pictureBox3
+            // pictureBoxBrush
             // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(108, 31);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(25, 23);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
+            this.pictureBoxBrush.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxBrush.Image")));
+            this.pictureBoxBrush.Location = new System.Drawing.Point(108, 31);
+            this.pictureBoxBrush.Name = "pictureBoxBrush";
+            this.pictureBoxBrush.Size = new System.Drawing.Size(25, 23);
+            this.pictureBoxBrush.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxBrush.TabIndex = 2;
+            this.pictureBoxBrush.TabStop = false;
+            this.pictureBoxBrush.Click += new System.EventHandler(this.ChangeItem);
             // 
             // pictureEraser
             // 
@@ -92,7 +104,7 @@
             this.pictureEraser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureEraser.TabIndex = 1;
             this.pictureEraser.TabStop = false;
-            this.pictureEraser.Click += new System.EventHandler(this.pictureEraser_Click);
+            this.pictureEraser.Click += new System.EventHandler(this.ChangeItem);
             // 
             // picturePen
             // 
@@ -106,19 +118,49 @@
             this.picturePen.TabIndex = 0;
             this.picturePen.TabStop = false;
             this.picturePen.WaitOnLoad = true;
-            this.picturePen.Click += new System.EventHandler(this.picturePen_Click);
+            this.picturePen.Click += new System.EventHandler(this.ChangeItem);
             // 
             // groupBoxColor
             // 
+            this.groupBoxColor.Controls.Add(this.panelPurple);
+            this.groupBoxColor.Controls.Add(this.panelYellow);
+            this.groupBoxColor.Controls.Add(this.panelGreen);
             this.groupBoxColor.Controls.Add(this.panelBlue);
             this.groupBoxColor.Controls.Add(this.panelRed);
             this.groupBoxColor.Controls.Add(this.panelBlack);
             this.groupBoxColor.Location = new System.Drawing.Point(170, 13);
             this.groupBoxColor.Name = "groupBoxColor";
-            this.groupBoxColor.Size = new System.Drawing.Size(150, 75);
+            this.groupBoxColor.Size = new System.Drawing.Size(73, 75);
             this.groupBoxColor.TabIndex = 2;
             this.groupBoxColor.TabStop = false;
             this.groupBoxColor.Text = "Цвет";
+            // 
+            // panelPurple
+            // 
+            this.panelPurple.BackColor = System.Drawing.Color.Purple;
+            this.panelPurple.Location = new System.Drawing.Point(50, 43);
+            this.panelPurple.Name = "panelPurple";
+            this.panelPurple.Size = new System.Drawing.Size(15, 15);
+            this.panelPurple.TabIndex = 5;
+            this.panelPurple.Click += new System.EventHandler(this.ChangeColor);
+            // 
+            // panelYellow
+            // 
+            this.panelYellow.BackColor = System.Drawing.Color.Yellow;
+            this.panelYellow.Location = new System.Drawing.Point(49, 21);
+            this.panelYellow.Name = "panelYellow";
+            this.panelYellow.Size = new System.Drawing.Size(15, 15);
+            this.panelYellow.TabIndex = 4;
+            this.panelYellow.Click += new System.EventHandler(this.ChangeColor);
+            // 
+            // panelGreen
+            // 
+            this.panelGreen.BackColor = System.Drawing.Color.Green;
+            this.panelGreen.Location = new System.Drawing.Point(28, 43);
+            this.panelGreen.Name = "panelGreen";
+            this.panelGreen.Size = new System.Drawing.Size(15, 15);
+            this.panelGreen.TabIndex = 3;
+            this.panelGreen.Click += new System.EventHandler(this.ChangeColor);
             // 
             // panelBlue
             // 
@@ -127,7 +169,7 @@
             this.panelBlue.Name = "panelBlue";
             this.panelBlue.Size = new System.Drawing.Size(15, 15);
             this.panelBlue.TabIndex = 2;
-            this.panelBlue.Click += new System.EventHandler(this.panelBlue_Click);
+            this.panelBlue.Click += new System.EventHandler(this.ChangeColor);
             // 
             // panelRed
             // 
@@ -136,7 +178,7 @@
             this.panelRed.Name = "panelRed";
             this.panelRed.Size = new System.Drawing.Size(15, 15);
             this.panelRed.TabIndex = 1;
-            this.panelRed.Click += new System.EventHandler(this.panelRed_Click);
+            this.panelRed.Click += new System.EventHandler(this.ChangeColor);
             // 
             // panelBlack
             // 
@@ -145,7 +187,7 @@
             this.panelBlack.Name = "panelBlack";
             this.panelBlack.Size = new System.Drawing.Size(15, 15);
             this.panelBlack.TabIndex = 0;
-            this.panelBlack.Click += new System.EventHandler(this.panelBlack_Click);
+            this.panelBlack.Click += new System.EventHandler(this.ChangeColor);
             // 
             // comboBoxPtt
             // 
@@ -160,42 +202,94 @@
             "7",
             "8",
             "9"});
-            this.comboBoxPtt.Location = new System.Drawing.Point(326, 25);
+            this.comboBoxPtt.Location = new System.Drawing.Point(249, 25);
             this.comboBoxPtt.Name = "comboBoxPtt";
-            this.comboBoxPtt.Size = new System.Drawing.Size(35, 24);
+            this.comboBoxPtt.Size = new System.Drawing.Size(55, 24);
             this.comboBoxPtt.TabIndex = 3;
             this.comboBoxPtt.SelectedIndexChanged += new System.EventHandler(this.comboBoxPtt_SelectedIndexChanged);
             // 
             // buttonClear
             // 
-            this.buttonClear.Location = new System.Drawing.Point(327, 56);
+            this.buttonClear.Location = new System.Drawing.Point(249, 55);
             this.buttonClear.Name = "buttonClear";
-            this.buttonClear.Size = new System.Drawing.Size(75, 25);
+            this.buttonClear.Size = new System.Drawing.Size(55, 25);
             this.buttonClear.TabIndex = 4;
             this.buttonClear.Text = "Clear";
             this.buttonClear.UseVisualStyleBackColor = true;
             this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBoxLine);
+            this.groupBox1.Controls.Add(this.pictureBoxRectangle);
+            this.groupBox1.Location = new System.Drawing.Point(310, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(160, 75);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Фигуры";
+            // 
+            // pictureBoxLine
+            // 
+            this.pictureBoxLine.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLine.Image")));
+            this.pictureBoxLine.Location = new System.Drawing.Point(36, 21);
+            this.pictureBoxLine.Name = "pictureBoxLine";
+            this.pictureBoxLine.Size = new System.Drawing.Size(24, 20);
+            this.pictureBoxLine.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxLine.TabIndex = 1;
+            this.pictureBoxLine.TabStop = false;
+            this.pictureBoxLine.Click += new System.EventHandler(this.ChangeFigure);
+            // 
+            // pictureBoxRectangle
+            // 
+            this.pictureBoxRectangle.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxRectangle.Image")));
+            this.pictureBoxRectangle.Location = new System.Drawing.Point(6, 21);
+            this.pictureBoxRectangle.Name = "pictureBoxRectangle";
+            this.pictureBoxRectangle.Size = new System.Drawing.Size(24, 20);
+            this.pictureBoxRectangle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxRectangle.TabIndex = 0;
+            this.pictureBoxRectangle.TabStop = false;
+            this.pictureBoxRectangle.Click += new System.EventHandler(this.ChangeFigure);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(477, 13);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 5;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(732, 453);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1532, 708);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.buttonClear);
             this.Controls.Add(this.comboBoxPtt);
             this.Controls.Add(this.groupBoxColor);
             this.Controls.Add(this.groupBoxTools);
             this.Controls.Add(this.panelMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(750, 500);
             this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.Text = "Paint";
-            //this.Load += new System.EventHandler(this.Form1_Load);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBoxTools.ResumeLayout(false);
             this.groupBoxTools.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBrush)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureEraser)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picturePen)).EndInit();
             this.groupBoxColor.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLine)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRectangle)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -206,13 +300,20 @@
         private System.Windows.Forms.GroupBox groupBoxTools;
         private System.Windows.Forms.GroupBox groupBoxColor;
         private System.Windows.Forms.PictureBox picturePen;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBoxBrush;
         private System.Windows.Forms.PictureBox pictureEraser;
         private System.Windows.Forms.Panel panelBlack;
         private System.Windows.Forms.Panel panelRed;
         private System.Windows.Forms.Panel panelBlue;
         private System.Windows.Forms.ComboBox comboBoxPtt;
         private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Panel panelGreen;
+        private System.Windows.Forms.Panel panelYellow;
+        private System.Windows.Forms.Panel panelPurple;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBoxLine;
+        private System.Windows.Forms.PictureBox pictureBoxRectangle;
+        private System.Windows.Forms.Button buttonSave;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,79 +17,150 @@ namespace Paint
         {
             InitializeComponent();
         }
-        Graphics g;
+
+        private Graphics g,gimg;
         bool created = false, paint = false;
-        int lastX, lastY;
-        Pen p = new Pen(Color.Black, 3);
-        Brush b;
+        private enum Item { Pen,Brush }
+        private Item PickedItem;
+        private Size sz;
 
-        private void panelMain_MouseUp(object sender, MouseEventArgs e)
-        {
-            paint = false;
-        }
+        private enum Figure { Line,Rectangle}
+        private Figure PickedFigure;
 
-        private void panelMain_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (paint)
-            {
-                g.DrawLine(p,lastX, lastY, e.X, e.Y);
-                lastX = e.X;
-                lastY = e.Y;
-            }
-        }
+        private Point startPoint,currentPoint;
+        private Pen p = new Pen(Color.Black, 3);
+        private Brush b = new SolidBrush(Color.Aquamarine);
 
-        private void panelBlack_Click(object sender, EventArgs e)
-        {
-            p.Color = Color.Black;
-        }
-
-        private void panelRed_Click(object sender, EventArgs e)
-        {
-            p.Color = Color.Red;
-        }
-
-        private void pictureEraser_Click(object sender, EventArgs e)
-        {
-            p.Color = panelMain.BackColor;
-        }
-
-        private void picturePen_Click(object sender, EventArgs e)
-        {
-            p.Color = Color.Black;
-        }
-
-        private void comboBoxPtt_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            p.Width = comboBoxPtt.SelectedIndex;
-        }
-
-        private void buttonClear_Click(object sender, EventArgs e)
-        {
-            panelMain.Refresh();
-        }
-
-        private void panelBlue_Click(object sender, EventArgs e)
-        {
-            p.Color = Color.Blue;
-        }
-
+        private Image img = null;
+        private BufferedGraphicsContext bgc =BufferedGraphicsManager.Current;
+        private BufferedGraphics bg;
 
         private void panelMain_MouseDown(object sender, MouseEventArgs e)
         {
             if (created)
             {
                 paint = true;
-                lastX = e.X;
-                lastY = e.Y;
+                startPoint = new Point(e.X, e.Y);
             }
             else
             {
                 g = panelMain.CreateGraphics();
                 paint = true;
                 created = true;
-                lastX = e.X;
-                lastY = e.Y;
+                PickedItem = Item.Pen;
+                PickedFigure = Figure.Line;
+                startPoint = new Point(e.X, e.Y);
             }
+        }
+        private void panelMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (img == null) return;
+            var bgg = bg.Graphics;
+            if (paint)
+            {
+                currentPoint = new Point(e.X, e.Y);
+                switch (PickedFigure)
+                {
+                    case Figure.Line:
+                        gimg.DrawLine(p, startPoint, currentPoint);
+                        g.DrawLine(p, startPoint, currentPoint);
+                        startPoint = currentPoint;
+                        break;
+                    case Figure.Rectangle:
+                        sz = new Size(currentPoint.X - startPoint.X,currentPoint.Y - startPoint.Y);
+                        if (p.Color == (pictureEraser).BackColor) p.Color = Color.Black;
+                        bgg.DrawImage(img, 0, 0);
+                        bgg.DrawRectangle(p, new Rectangle(startPoint, sz));
+                        bg.Render();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        private void panelMain_MouseUp(object sender, MouseEventArgs e)
+        {
+            paint = false;
+            sz = new Size(currentPoint.X - startPoint.X, currentPoint.Y - startPoint.Y);
+            switch (PickedFigure)
+            {
+                case Figure.Rectangle:
+                    gimg.DrawRectangle(p, new Rectangle(startPoint, sz));
+                    g.DrawRectangle(p, new Rectangle(startPoint, sz));
+                    //можно добавить заливку в g и gimg кисточкой
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ChangeColor(object sender,EventArgs e)
+        {
+            p.Color = ((Panel)sender).BackColor;
+        }
+        private void ChangeFigure(object sender,EventArgs e)
+        {
+            switch (((PictureBox)sender).Name)
+            {
+                case "pictureBoxRectangle":
+                    PickedFigure = Figure.Rectangle;
+                    break;
+                case "pictureBoxLine":
+                    PickedFigure=Figure.Line;
+                    break;
+            }
+        }
+        private void ChangeItem(object sender,EventArgs e)
+        {
+            switch (((PictureBox)sender).Name)
+            {
+                case "picturePen":
+                    PickedFigure = Figure.Line;
+                    PickedItem = Item.Pen;
+                    p.Color = Color.Black;
+                    break;
+                case "pictureEraser":
+                    PickedFigure = Figure.Line;
+                    PickedItem = Item.Pen;
+                    p.Color = ((PictureBox)sender).BackColor;
+                    break;
+                case "pictureBrush":
+                    //PickedFigure = Figure.Line;
+                    break;
+            }
+        }
+        private void comboBoxPtt_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            p.Width = comboBoxPtt.SelectedIndex;
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            img.Save("picture.jpg", ImageFormat.Jpeg);
+        }
+
+        private void panelMain_Paint(object sender, PaintEventArgs e)
+        {
+            if (img == null)
+            {
+                img = new Bitmap(panelMain.Width, panelMain.Height, panelMain.CreateGraphics());
+                gimg = Graphics.FromImage(img);
+                gimg.Clear(pictureEraser.BackColor);
+                bg = bgc.Allocate(panelMain.CreateGraphics(), new Rectangle(0, 0, panelMain.Width, panelMain.Height));
+            }
+            e.Graphics.DrawImage(img, 0, 0);
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            panelMain.Refresh();
+            gimg.Clear(panelMain.BackColor);
+            g.Clear(panelMain.BackColor);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
